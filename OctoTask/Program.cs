8b2ea0e1b3f5c102d1c08e9d2e0499cac91b45a1
JEDIBASIL <jedidiahbasil@gmail.com>
2022-10-21 15:58:36 +0100

    thank you sir

--- OctoTask/Program.cs
+++ OctoTask/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Linq;
+using OctoTask.algorithms;
 
 namespace OctoTask
 {
@@ -7,6 +10,19 @@ namespace OctoTask
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Palidrone palidrone = new Palidrone();
+            MinAndMaxNumber minAndMaxNumber = new MinAndMaxNumber();
+            GetDuplicates getDuplicates = new GetDuplicates();
+            SequenceCounter sequenceCounter = new SequenceCounter();
+
+            //Console.WriteLine(palidrone.PaliTest("saippuakivikauppias"));
+            //Console.WriteLine(minAndMaxNumber.FindMinAndMaxNumber(new int[] {5,4,346,98,2 }));
+            //Console.WriteLine(getDuplicates.FindDuplicates(new int[] { 5, 4, 346, 98, 5,9,9,9, 2 }));
+           Hashtable sequenceCheck = sequenceCounter.FindSequenceCounter(new int[] { 5, 4, 346, 98, 5, 9, 9, 9, 2 });
+
+            foreach(DictionaryEntry entry in sequenceCheck){
+                Console.WriteLine(entry.Value+": "+entry.Key);
+            }
         }
     }
 }
---
+++ OctoTask/algorithms/MinAndMaxNumber.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace OctoTask.algorithms
+{
+    public class MinAndMaxNumber
+    {
+        public MinAndMaxNumber()
+        {
+        }
+
+        public string FindMinAndMaxNumber(int[] numArray)
+        {
+            int min = numArray.Min();
+            int max = numArray.Max();
+            return "{"+min+", "+max+"}";
+        }
+    }
+}
